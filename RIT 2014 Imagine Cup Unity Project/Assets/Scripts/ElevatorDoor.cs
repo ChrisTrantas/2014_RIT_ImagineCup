a41d90263337613dc8ef6a6e9e92fb31f07fde8f
Michael Cooper <mtc5284@rit.edu>
2014-11-15 18:53:04 -0500

    Changes to the elevator and the elevator door

---
+++ RIT 2014 Imagine Cup Unity Project/Assets/Scripts/ElevatorDoor.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorDoor : MonoBehaviour {
+	public bool closed = true;
+	Vector3 closedPosition;
+	public Vector3 direction = Vector3.left;
+	public float distance;
+	public float duration = 4;
+
+	bool opening, closing;
+
+	// Use this for initialization
+	void Start () {
+		closedPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (opening) {
+			transform.position += direction * distance / 4 * Time.deltaTime;
+			if (Vector3.Distance(transform.position, closedPosition) > distance) {
+				opening = false;
+				closed = false;
+			}
+		} else if (closing) {
+			transform.position -= direction * distance / 4 * Time.deltaTime;
+			if ((transform.position - closedPosition).magnitude < 0) {
+				closing = false;
+				closed = true;
+			}
+		}
+	}
+
+	public void open()	{
+		if(closed)
+			opening = true;
+	}
+
+	public void close(){
+		if(!closed)
+			closing = true;
+	}
+}
---
+++ RIT 2014 Imagine Cup Unity Project/Assets/Scripts/TriggerElevatorDoors.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerElevatorDoors : MonoBehaviour {
+	public GameObject[] elevatorDoors;
+	public float delay;
+	public bool openDoor;
+	bool triggered = false;
+	float timer = 0;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (triggered) {
+			timer += Time.deltaTime;
+			if(timer > delay){
+				if(openDoor){
+					foreach(GameObject elevatorDoor in elevatorDoors){
+						elevatorDoor.GetComponent<ElevatorDoor>().open();
+					}
+				}
+				else {
+					foreach(GameObject elevatorDoor in elevatorDoors){
+						elevatorDoor.GetComponent<ElevatorDoor>().close();
+					}
+				}
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		triggered = true;
+	}
+}
