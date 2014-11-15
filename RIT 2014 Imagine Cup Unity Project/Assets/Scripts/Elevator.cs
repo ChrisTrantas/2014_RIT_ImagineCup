02881aa691bd8bd1e09260ba7a881df380edda96
Chris Trantas <cmt4536@g.rit.edu>
2014-11-15 06:52:48 -0500

    Test for 2nd level

--- RIT 2014 Imagine Cup Unity Project/Assets/Scripts/Elevator.cs
+++ RIT 2014 Imagine Cup Unity Project/Assets/Scripts/Elevator.cs
@@ -5,11 +5,20 @@ public class Elevator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		GoingDown ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+	if(
 	}
+
+	void GoingDown(){
+
+			if (KeyCode == 'e') 
+			{
+						audio.Play ();
+						Application.LoadLevel ("1st Level");
+				}
+		}
 }
