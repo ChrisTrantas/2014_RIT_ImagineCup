4a665efa4b898b65da05351a387c7257ecc1d37e
Chris Trantas <cmt4536@g.rit.edu>
2014-11-15 10:20:42 -0500

    Shit got done. E works.

---
+++ RIT 2014 Imagine Cup Unity Project/Assets/Scripts/LevelChecks.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelChecks : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void LightCheck(){
+		// Check plane/Mesh collider
+
+		// if collide destroy plane
+
+		// for loop to flicker the lights
+
+	}
+
+	void DoorCheck(){
+		// Check dist between door and controller
+
+		// if close and 'e' is pressed check to see if it's open or closed
+
+		// open or closed check
+
+		//rotate the door
+	}
+}
