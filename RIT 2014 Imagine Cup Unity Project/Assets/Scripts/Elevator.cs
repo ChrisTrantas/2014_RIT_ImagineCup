bc447293cb4cb2d85f9f10c156182df4e7372fa9
lms7898 <lms7898@rit.edu>
2014-11-15 11:41:34 -0500

    Finished raycasting to activate objects in line of sight with "E"

--- RIT 2014 Imagine Cup Unity Project/Assets/Scripts/Elevator.cs
+++ RIT 2014 Imagine Cup Unity Project/Assets/Scripts/Elevator.cs
@@ -5,12 +5,26 @@ public class Elevator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		audio.loop = false;
+		//audio.loop = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		GoingDown ();
+		Ray ray;
+		RaycastHit hit;
+		ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+
+		Debug.DrawRay (ray.origin, ray.direction * 10, Color.cyan);
+
+
+		if (Physics.Raycast (ray, out hit)) {
+			print ("I'm looking at " + hit.transform.name);
+			if(hit.transform.name == "Panel")
+			{
+				GoingDown ();
+			}
+		}
+
 	}
 
 	void GoingDown(){
@@ -25,7 +39,7 @@ public class Elevator : MonoBehaviour {
 						//Turn Off Directional Light
 
 						// Play sound maybe?
-						audio.loop = true;
+						//audio.loop = true;
 						// Load the next scene
 						Application.LoadLevel ("1st Level");
 						//Object.DontDestroyOnLoad (CharacterController);
