3b22c7c445bc54dda237ed05e4a6bf0d101102f7
Michael Cooper <mtc5284@rit.edu>
2014-11-15 11:54:50 -0500

    Created a way to trigger lights.

---
+++ RIT 2014 Imagine Cup Unity Project/Assets/Scripts/triggerLight.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class triggerLight : MonoBehaviour {
+	public Light[] lights;
+	bool triggered = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (triggered) {
+			foreach(Light light in lights)
+			{
+				light.enabled = false;
+			}
+		}
+	}
+
+	void OnTriggerEnter(Collider other) {
+		foreach(Light light in lights)
+		{
+			light.intensity = (Random.Range(0.5));
+		}
+	}
+}
